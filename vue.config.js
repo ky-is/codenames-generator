9c12fd47dd8ecf7bd0dc363657c65254d3dd0258
Kyle Coburn <kylecoburn@gmail.com>
2019-01-01 21:44:47 -0800

    🏗

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+	baseUrl: process.env.NODE_ENV === 'production' ? '/codenames' : '/',
 	productionSourceMap: false,
 	// whitespace: 'condense',
+	devServer: {
+		open: true,
+		port: 8081,
+	},
 }
